a808feb401ad81425ee017182ff8411cd4aaca07
raiin <onlinecoinbroker@gmail.com>
2020-06-03 17:38:32 -0400

    insta-wallet-v1

---
+++ InstantWallet/InstantWallet/Controllers/WalletController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InstantWallet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NBitcoin;
+
+namespace InstantWallet.Controllers
+{
+    public class WalletController : Controller
+    {
+        // GET: WalletController
+        public ActionResult Index()
+        {
+
+            //IQueryable<Wallet> wallet;
+            var wallet = new Wallet();
+
+            wallet.generateKey();
+
+            return View(wallet);
+        }
+
+        // GET: WalletController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WalletController/Create
+        public ActionResult Create()
+        {
+            return View(new Wallet());
+        }
+
+        // POST: WalletController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Wallet wallet)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    //var freshWallet = new Wallet();
+
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            catch
+            {
+                return View(wallet);
+            }
+
+            return View(wallet);
+
+        }
+                
+
+        // GET: WalletController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WalletController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalletController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WalletController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ InstantWallet/InstantWallet/Models/Wallet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NBitcoin;
+using NBitcoin.RPC;
+
+namespace InstantWallet.Models
+{
+    public class Wallet
+    {
+
+        public string myPublicAddress; // public btc address (For Receiving funds)
+        public string myPrivateKey; // private btc address (For Spending Funds)
+        public string mnemo; // backup recovery seed phrase
+
+
+        public void generateKey()
+        {
+
+            Key privateKey = new Key(); //generate new key
+            PubKey publicKey = privateKey.PubKey; //get public key
+            myPublicAddress = publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString(); //convert public key to string
+
+            Key privateKey2 = new Key(); //create private key
+            BitcoinSecret mainNetPrivateKey = privateKey2.GetBitcoinSecret(Network.Main); //get the secret key
+            myPrivateKey = mainNetPrivateKey.ToString(); // convert private key to string
+
+            mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve).ToString(); //create word list and convert to string
+
+        }
+    }
+    
+}
